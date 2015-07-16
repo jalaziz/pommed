5cc2bcca370fd24fb474d3f1a9dbc84f140fa63c
James An <james@jamesan.ca>
2015-07-16 08:08:49 -0400

    Added support for MacBookPro10,2, MacBookPro11,3 and MacbookAir6,2, added additional Apple USB device IDs and corrected device names according to usb.ids.

--- pommed/evdev.h
+++ pommed/evdev.h
@@ -42,9 +42,13 @@
 #define USB_PRODUCT_ID_GEYSER4_JIS    0x021c
 
 /* Apple Geyser IV-HF keyboard + trackpad */
-#define USB_PRODUCT_ID_GEYSER4HF_ANSI   0x0229
-#define USB_PRODUCT_ID_GEYSER4HF_ISO    0x022a
-#define USB_PRODUCT_ID_GEYSER4HF_JIS    0x022b
+#define USB_PRODUCT_ID_GEYSER4_HF_ANSI   0x0229
+#define USB_PRODUCT_ID_GEYSER4_HF_ISO    0x022a
+#define USB_PRODUCT_ID_GEYSER4_HF_JIS    0x022b
+
+/* Apple Fountain or Geyser trackpad only */
+#define USB_PRODUCT_ID_FOUNTAIN_TP_ONLY   0x030a
+#define USB_PRODUCT_ID_GEYSER1_TP_ONLY    0x030b
 
 /* WellSpring devices : AppleUSBMultitouch.kext */
 
@@ -83,38 +87,70 @@
 #define USB_PRODUCT_ID_WELLSPRING5A_ISO   0x0253
 #define USB_PRODUCT_ID_WELLSPRING5A_JIS   0x0254
 
+/* Apple WellSpring VI keyboard + trackpad */
+#define USB_PRODUCT_ID_WELLSPRING6_ANSI    0x024c
+#define USB_PRODUCT_ID_WELLSPRING6_ISO     0x024d
+#define USB_PRODUCT_ID_WELLSPRING6_JIS     0x024e
+
+/* Apple WellSpring VIa keyboard + trackpad */
+#define USB_PRODUCT_ID_WELLSPRING6A_ANSI   0x0249
+#define USB_PRODUCT_ID_WELLSPRING6A_ISO    0x024a
+#define USB_PRODUCT_ID_WELLSPRING6A_JIS    0x024b
+
  /* Apple WellSpring VII keyboard + trackpad */
 #define USB_PRODUCT_ID_WELLSPRING7_ANSI  0x0262
 #define USB_PRODUCT_ID_WELLSPRING7_ISO   0x0263
 #define USB_PRODUCT_ID_WELLSPRING7_JIS   0x0264
 
+ /* Apple WellSpring VIIa keyboard + trackpad */
+#define USB_PRODUCT_ID_WELLSPRING7A_ANSI   0x0259
+#define USB_PRODUCT_ID_WELLSPRING7A_ISO    0x025a
+#define USB_PRODUCT_ID_WELLSPRING7A_JIS    0x025b
+
+ /* Apple WellSpring VIII keyboard + trackpad */
+#define USB_PRODUCT_ID_WELLSPRING8_ANSI    0x0290
+#define USB_PRODUCT_ID_WELLSPRING8_ISO     0x0291
+#define USB_PRODUCT_ID_WELLSPRING8_JIS     0x0292
+
 /* Apple external USB keyboard, white */
-#define USB_PRODUCT_ID_APPLE_EXTKBD_WHITE   0x020c
+#define USB_PRODUCT_ID_EXTKBD_WHITE   0x020c
 
 /* Apple external USB mini keyboard, aluminium */
-#define USB_PRODUCT_ID_APPLE_EXTKBD_MINI_ALU_ANSI 0x021d
-#define USB_PRODUCT_ID_APPLE_EXTKBD_MINI_ALU_ISO  0x021e
-#define USB_PRODUCT_ID_APPLE_EXTKBD_MINI_ALU_JIS  0x021f
+#define USB_PRODUCT_ID_EXTKBD_ALU_MINI_ANSI 0x021d
+#define USB_PRODUCT_ID_EXTKBD_ALU_MINI_ISO  0x021e
+#define USB_PRODUCT_ID_EXTKBD_ALU_MINI_JIS  0x021f
 
 /* Apple external USB keyboard, aluminium */
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_ANSI 0x0220
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_ISO  0x0221
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_JIS  0x0222
+#define USB_PRODUCT_ID_EXTKBD_ALU_ANSI 0x0220
+#define USB_PRODUCT_ID_EXTKBD_ALU_ISO  0x0221
+#define USB_PRODUCT_ID_EXTKBD_ALU_JIS  0x0222
+
+/* Apple external USB keyboard, aluminium, newer model */
+#define USB_PRODUCT_ID_EXTKBD_ALU_REVB_ANSI 0x024f
+#define USB_PRODUCT_ID_EXTKBD_ALU_REVB_ISO  0x0250
+#define USB_PRODUCT_ID_EXTKBD_ALU_REVB_JIS  0x0251
 
 /* Apple external wireless keyboard, aluminium */
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_ANSI 0x022c
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_ISO  0x022d
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_JIS  0x022e
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_ANSI 0x022c
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_ISO  0x022d
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_JIS  0x022e
 
-/* Apple external wireless keyboard, aluminium, newer model */
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_2_ANSI 0x0239
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_2_ISO  0x023a
-#define USB_PRODUCT_ID_APPLE_EXTKBD_ALU_WL_2_JIS  0x023b
+/* Apple external wireless keyboard, aluminium, 2009 model */
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2009_ANSI 0x0239
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2009_ISO  0x023a
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2009_JIS  0x023b
+
+/* Apple external wireless keyboard, aluminium, 2011 model */
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2011_ANSI 0x0255
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2011_ISO  0x0256
+#define USB_PRODUCT_ID_EXTKBD_ALU_WL_2011_JIS  0x0257
 
 /* Apple Remote IR Receiver */
 #define USB_PRODUCT_ID_APPLEIR        0x8240
-#define USB_PRODUCT_ID_APPLEIR_2      0x8242
-
+#define USB_PRODUCT_ID_APPLEIR_2      0x1440
+#define USB_PRODUCT_ID_APPLEIR_3      0x8241
+#define USB_PRODUCT_ID_APPLEIR_4      0x8242
+#define USB_PRODUCT_ID_APPLEIR_5      0x8243
 
 #define EVDEV_DIR               "/dev/input"
 #define EVDEV_BASE              "/dev/input/event"
--- pommed/pommed.c
+++ pommed/pommed.c
@@ -325,8 +325,18 @@ struct machine_ops mb_mops[] = {
     /* .evdev_identify = evdev_is_wellspring5a, */
   },
 
-  { /* MacBookPro10,1 (15" Retina, Mid 2012) */
+  { /* MacBookPro10,1 (15" Retina, Mid 2012)
+     * MacBookPro10,2 (13" Retina Early/Late 2013)
+     */
     .type = MACHINE_MACBOOKPRO_10,
+    .lcd_backlight_probe = mbp_sysfs_backlight_probe,
+    .lcd_backlight_step = sysfs_backlight_step,
+    .lcd_backlight_toggle = sysfs_backlight_toggle,
+    /* .evdev_identify = evdev_is_wellspring7a / evdev_is_wellspring7, */
+  },
+
+  { /* MacBookPro11,3 (15" Retina, Late 2013) */
+    .type = MACHINE_MACBOOKPRO_11,
     .lcd_backlight_probe = mbp_sysfs_backlight_probe,
     .lcd_backlight_step = sysfs_backlight_step,
     .lcd_backlight_toggle = sysfs_backlight_toggle,
@@ -415,6 +425,14 @@ struct machine_ops mb_mops[] = {
     .lcd_backlight_step = sysfs_backlight_step,
     .lcd_backlight_toggle = sysfs_backlight_toggle,
     /* .evdev_identify = evdev_is_wellspring4a / evdev_is_wellspring4, */
+  },
+
+  {  /* MacbookAir6,2 (13" Mid 2013) */
+    .type = MACHINE_MACBOOKAIR_6,
+    .lcd_backlight_probe = mbp_sysfs_backlight_probe,
+    .lcd_backlight_step = sysfs_backlight_step,
+    .lcd_backlight_toggle = sysfs_backlight_toggle,
+    /* .evdev_identify = evdev_is_wellspring8, */
   }
 };
 #endif /* __powerpc__ */
@@ -778,9 +796,15 @@ check_machine_dmi(void)
   else if ((strcmp(buf, "MacBookPro9,1") == 0)
      || (strcmp(buf, "MacBookPro9,2") == 0))
     ret = MACHINE_MACBOOKPRO_9;
-  /* Macbook Pro 15" Retina (Mid 2012) */
-  else if ((strcmp(buf, "MacBookPro10,1") == 0))
+  /* Macbook Pro 15" Retina (Mid 2012)
+   * Macbook Pro 13" Retina (Early/Late 2013)
+   */
+  else if ((strcmp(buf, "MacBookPro10,1") == 0)
+     || (strcmp(buf, "MacBookPro10,2") == 0))
     ret = MACHINE_MACBOOKPRO_10;
+  /* Macbook Pro 15" Retina (Late 2013) */
+  else if ((strcmp(buf, "MacBookPro11,3") == 0))
+    ret = MACHINE_MACBOOKPRO_11;
   /* Core Duo MacBook (May 2006) */
   else if (strcmp(buf, "MacBook1,1") == 0)
     ret = MACHINE_MACBOOK_1;
@@ -811,6 +835,9 @@ check_machine_dmi(void)
   /* MacBook Air 11" & 13" (October 2010) */
   else if ((strcmp(buf, "MacBookAir3,1") == 0) || (strcmp(buf, "MacBookAir3,2") == 0))
     ret = MACHINE_MACBOOKAIR_3;
+  /* MacBook Air 13" (Mid 2013) */
+  else if (strcmp(buf, "MacBookAir6,2") == 0)
+    ret = MACHINE_MACBOOKAIR_6;
   else
     logmsg(LOG_ERR, "Unknown Apple machine: %s", buf);
 
